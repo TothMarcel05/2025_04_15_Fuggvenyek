5460c5eae99bd26bf7b31fdd1bf889f16c481cf8
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2025-04-15 10:37:19 +0200

    .

---
+++ js.js
@@ -0,0 +1,34 @@
+// let add = function (a, b) 
+// { return a + b;  }
+//     console.log(add(3, 2));
+
+// let add = (a, b) => a + b;
+// console.log(add(5, 3));
+
+// (function() {
+//     let nev = prompt("(Név)"); udv = function() 
+//     { return "Szia! " + nev + " vagyok!";}
+
+
+// console.log(udv());}());
+// console.log(nev);
+
+
+function nevek(){
+    let nev = prompt("Írd be a neved", "...");
+    document.getElementById("szoveg1").innerHTML = "Szia " + nev + " vagyok!";
+}
+
+
+
+
+
+    
+
+
+// let elem = document.getElementById("elem");
+// console.log(elem);
+
+
+
+
